ed80077b261ed6d519edd7f0c733f78fb7e0a5f9
thoai nguyen <thoaikun@gmail.com>
2022-10-30 22:44:13 +0700

    update

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,4 +1,4 @@
-import { DataSignIn } from "../types";
+import { DataSignIn, DataSignUp } from "../types";
 import { useState } from "react";
 import UserService from "../services/userApi";
 import { useNavigate } from "react-router-dom";
@@ -13,7 +13,18 @@ const useAuth = () => {
     localStorage.removeItem("_refresh_token");
     navigate("/login");
   };
-
+  const onRegister = async (value: DataSignUp) => {
+    try {
+      setLoading(true);
+      const { data } = await UserService.register(value);
+      localStorage.setItem("_token", data.accessToken);
+      localStorage.setItem("_refresh_token", data.refreshAccessToken);
+      navigate("/");
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+    }
+  };
   const onLogin = async (value: DataSignIn) => {
     try {
       setLoading(true);
@@ -27,7 +38,7 @@ const useAuth = () => {
     }
   };
 
-  return { loading, error, onLogin, onLogout };
+  return { loading, error, onLogin, onLogout, onRegister };
 };
 
 export default useAuth;
--- src/page/register/Register.tsx
+++ src/page/register/Register.tsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from "react";
+import Styles from "./style.module.scss";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import { MdPhoneIphone } from "react-icons/md";
+import { Si1Password } from "react-icons/si";
+import { AiOutlineMail } from "react-icons/ai";
+import { RiLockPasswordFill } from "react-icons/ri";
+import CustomInput from "@/components/common/customInput/CustomInput";
+import useAuth from "@/hooks/useAuth";
 
 function Register() {
+  const { onRegister } = useAuth();
+  const formik = useFormik({
+    initialValues: {
+      phone: "0865132893",
+      email: "thoaikun1905@gmail.com",
+      confirm_password: "thoai1905",
+      password: "thoai1905",
+    },
+    onSubmit: (values) => {
+      onRegister(values);
+    },
+  });
   return (
-    <div>Register</div>
-  )
+    <div className={Styles.main}>
+      {" "}
+      <div className={`${Styles.headerTitle} mt-40`}>Zalo</div>
+      <div className={`${Styles.title} mb-20`}>
+        Đăng ký tài khoản Zalo trên Web
+      </div>
+      <div className={`${Styles.form} mt-20`}>
+        <form onSubmit={formik.handleSubmit}>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<MdPhoneIphone size={20} />}
+              placeholder="Số điện thoại"
+              onChange={formik.handleChange}
+              // value={formik.values.phone}
+              name="phone"
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<AiOutlineMail size={20} />}
+              placeholder="Email"
+              type="email"
+              onChange={formik.handleChange}
+              name="email"
+              // value={formik.values.password}
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<RiLockPasswordFill size={20} />}
+              placeholder="Mật khẩu"
+              type="password"
+              onChange={formik.handleChange}
+              name="password"
+              // value={formik.values.password}
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<Si1Password size={20} />}
+              placeholder="Xác nhận mật khẩu"
+              type="password"
+              onChange={formik.handleChange}
+              name="confirm_password"
+              // value={formik.values.password}
+            />
+          </div>
+          <button className={`${Styles.btn} ${Styles.btnLogin} mb-20`}>
+            Đăng ký
+          </button>
+
+          <div style={{ textAlign: "center", marginTop: "5px" }}>
+            Bạn đã có tài khoản ?<Link to="/login">Đăng nhập </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
