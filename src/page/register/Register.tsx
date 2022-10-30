cd776b83e2d34d60065f72770e9fba79387992d0
thoai nguyen <thoaikun@gmail.com>
2022-10-30 23:47:38 +0700

    update

--- src/page/register/Register.tsx
+++ src/page/register/Register.tsx
@@ -2,21 +2,31 @@ import React from "react";
 import Styles from "./style.module.scss";
 import { Link } from "react-router-dom";
 import { useFormik } from "formik";
-import { MdPhoneIphone } from "react-icons/md";
+import {
+  MdPhoneIphone,
+  MdOutlineDateRange,
+  MdOutlineDriveFileRenameOutline,
+} from "react-icons/md";
 import { Si1Password } from "react-icons/si";
+import moment from "moment";
 import { AiOutlineMail } from "react-icons/ai";
-import { RiLockPasswordFill } from "react-icons/ri";
+import { BsFillCalendarDateFill, BsGenderAmbiguous } from "react-icons/bs";
+import { TbUserMinus } from "react-icons/tb";
+import { RiLockPasswordFill, RiUser3Line } from "react-icons/ri";
+import { dataGender } from "../../mock/mockData";
 import CustomInput from "@/components/common/customInput/CustomInput";
+import CustomDate from "@/components/common/customDate/CustomDate";
+import CustomSelect from "@/components/common/customSelect/CustomSelect";
 import useAuth from "@/hooks/useAuth";
 
 function Register() {
   const { onRegister } = useAuth();
   const formik = useFormik({
     initialValues: {
-      phone: "0865132893",
-      email: "thoaikun1905@gmail.com",
-      confirm_password: "thoai1905",
-      password: "thoai1905",
+      phone: "",
+      email: "",
+      confirm_password: "",
+      password: "",
     },
     onSubmit: (values) => {
       onRegister(values);
@@ -29,8 +39,35 @@ function Register() {
       <div className={`${Styles.title} mb-20`}>
         Đăng ký tài khoản Zalo trên Web
       </div>
-      <div className={`${Styles.form} mt-20`}>
+      <div className={`${Styles.form}`}>
         <form onSubmit={formik.handleSubmit}>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<RiUser3Line size={20} />}
+              placeholder="Username"
+              onChange={formik.handleChange}
+              // value={formik.values.phone}
+              name="username"
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<MdOutlineDriveFileRenameOutline size={20} />}
+              placeholder="First Name"
+              onChange={formik.handleChange}
+              // value={formik.values.phone}
+              name="first_name"
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomInput
+              label={<TbUserMinus size={20} />}
+              placeholder="Last Name"
+              onChange={formik.handleChange}
+              // value={formik.values.phone}
+              name="last_name"
+            />
+          </div>
           <div className={Styles.inputForm}>
             <CustomInput
               label={<MdPhoneIphone size={20} />}
@@ -47,6 +84,34 @@ function Register() {
               type="email"
               onChange={formik.handleChange}
               name="email"
+              // value={formik.values.password}
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomSelect
+              label={<BsGenderAmbiguous size={20} />}
+              defaultValue="nam"
+              listOption={dataGender}
+              type="text"
+              onChange={(value: string) => {
+                formik.setFieldValue("gender", value);
+              }}
+              name="gender"
+              // value={formik.values.password}
+            />
+          </div>
+          <div className={Styles.inputForm}>
+            <CustomDate
+              label={<BsFillCalendarDateFill size={20} />}
+              placeholder="Birthday"
+              type="email"
+              onChange={(e: any) => {
+                formik.setFieldValue(
+                  "birthday",
+                  moment(e).format("MM-DD-YYYY")
+                );
+              }}
+              name="birthday"
               // value={formik.values.password}
             />
           </div>
